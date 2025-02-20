441f1586c6cf12d30198420c336ec83f64efa300
bb2636 <leesubin0322@gmail.com>
2025-02-20 16:32:10 +0900

    addDemo i=2

--- src/AddDemo.java
+++ src/AddDemo.java
@@ -1,7 +1,7 @@
 public class AddDemo {
   public static void main(String[] args) {
     int j = 0;
-    int i = 1;
+    int i = 2;
     System.out.println("연산전 : " + j );
     //j = ++i;  >>2
     j = i++; //참조 후 더하기 때문에 결과값에 1이 더해지지 않음
