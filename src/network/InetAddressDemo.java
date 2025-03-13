e7766fb89d62b72e00364d799571ac92bde49e49
bb2636 <leesubin0322@gmail.com>
2025-03-13 10:00:15 +0900

    inet 수정

--- src/network/InetAddressDemo.java
+++ src/network/InetAddressDemo.java
@@ -9,12 +9,14 @@ public class InetAddressDemo {
   public static void main(String[] args) {
     InetAddress address = null;
     InetAddress address2 = null;
+    InetAddress[] addresses = null;
     Scanner in = new Scanner(System.in);
     System.out.print("호스트 이름 입력 : ");
     String url = in.nextLine();
     try {
       address = InetAddress.getByName(url);
       address2 = InetAddress.getLocalHost();
+      addresses = InetAddress.getAllByName(url);
     } catch (UnknownHostException e) {
       System.out.println(e.getMessage());
     }
@@ -23,5 +25,8 @@ public class InetAddressDemo {
     System.out.println(url + "의 호스트 이름 : " + address.getHostName());
 
     System.out.println("로컬 호스트 주소 " +address2);
+    System.out.println(url + "의 ip 주소들은 총 " + addresses.length + " 개이며 ");
+    Arrays.stream(addresses)
+        .forEach(a -> System.out.println(a.getHostAddress()));
   }
 }
